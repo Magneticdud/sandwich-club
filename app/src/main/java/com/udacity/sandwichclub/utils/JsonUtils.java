ffbe551fc00180dd985ebcec89110ea10e1b3c6a
Magnetic_dud <mail@dandandin.it>
2018-03-23 23:02:40 +0100

    start to do something

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,10 +2,27 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONException;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        //it's empty, that's why it doesn't work
+        if (json==null) {
+            //if no data, don't try to parse it, and return null
+            return null;
+            //in mainactivity, another check for null will alert the user for this problem
+        }
+        //if we're here, that means the string is not empty, so it might contain a json
+        try {
+
+        }
+        catch (JSONException e){
+            //we got some big error, don't crash the whole app
+            //return null so the user has a nice error saying the problem with the
+            //sandwich data
+            return null;
+        }
+
         return null;
     }
 }
