d3da4b78c7f7e7d65c67f2354cb3591af487ca94
Magnetic_dud <mail@dandandin.it>
2018-03-23 23:22:57 +0100

    returns all the json

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,13 +2,23 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
+    //from the json
     static final String NAME = "name";
     static final String MAIN_NAME = "mainName";
+    static final String ALSO_KNOWN_AS = "alsoKnownAs";
+    static final String PLACE_OF_ORIGIN = "placeOfOrigin";
+    static final String DESCRIPTION = "description";
+    static final String IMAGE = "image";
+    static final String INGREDIENTS = "ingredients";
 
     public static Sandwich parseSandwichJson(String json) {
         if (json==null) {
@@ -19,9 +29,34 @@ public class JsonUtils {
         //if we're here, that means the string is not empty, so it might contain a json
         try {
             JSONObject sandwichJson = new JSONObject(json);
+            //create new sandwich object
             Sandwich sandwich = new Sandwich();
             JSONObject sandwichNameJson = sandwichJson.getJSONObject(NAME);
             sandwich.setMainName(sandwichNameJson.getString(MAIN_NAME));
+
+            //also known
+            JSONArray alsoKnownAs = sandwichName.getJSONArray(ALSO_KNOWN_AS);
+            List<String> alsoKnownNames = new ArrayList<>(alsoKnownAs.length());
+            for(int i=0; i< alsoKnownAs.length(); i++){
+                alsoKnownNames.add(alsoKnownAs.getString(i));
+            }
+            sandwich.setAlsoKnownAs(alsoKnownNames);
+
+            //description
+            sandwich.setDescription(sandwichJson.getString(DESCRIPTION));
+            //image
+            sandwich.setImage(sandwichJson.getString(IMAGE));
+            //origin
+            sandwich.setPlaceOfOrigin(sandwichJson.getString(PLACE_OF_ORIGIN));
+
+            //ingredients array
+            JSONArray ingredientsJson = sandwichJson.getJSONArray(INGREDIENTS);
+            List<String> ingredients = new ArrayList<>(ingredientsJson.length());
+            for(int i=0; i< ingredientsJson.length(); i++){
+                ingredients.add(ingredientsJson.getString(i));
+            }
+            sandwich.setIngredients(ingredients);
+            
             return sandwich;
         }
         catch (JSONException e){
